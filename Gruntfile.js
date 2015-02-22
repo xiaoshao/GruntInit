a1c0f4fa30b49db43f6a0ca4e4ab56cbed2aa40a
Shao Zengwei <zengwei19861029@163.com>
2015-02-22 09:56:22 +0800

    add load-grunt-config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,39 @@
 module.exports=function(grunt){
 	
-	//project configure
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
-			},
-			build: {
-				src: 'src/<%= pkg.name%>.js',
-				dest: 'build/<%= pkg.name%>.min.js'
-			}
-		}	
+	var path = require('path');
+
+	require('load-grunt-config')(grunt, {
+		configPath: path.join(process.cwd(), 'tasks'),
+		 data: {
+            pkg: grunt.file.readJSON('package.json')
+        },
+		init: true
+		// data:{test: false},
+		// loadGruntTasks: {
+		// 	pattern: 'grunt-*',
+		// 	config: require('./package.json'),
+		// 	scope: 'devDependencies'
+		// },
+		// postProcess:
 	});
+	//project configure
+	// grunt.initConfig({
+	// 	pkg: grunt.file.readJSON('package.json'),
+	// 	// uglify: {
+	// 	// 	options: {
+	// 	// 		banner: '/*! <%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
+	// 	// 	},
+	// 	// 	build: {
+	// 	// 		src: 'src/<%= pkg.name%>.js',
+	// 	// 		dest: 'build/<%= pkg.name%>.min.js'
+	// 	// 	}
+	// 	// },
+	// 	log: {
+	// 		foo: [1, 2, 3],
+	// 		bar: "hello world",
+	// 		baz: false
+	// 	}	
+	// });
 	
 	//loading task plugin
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -21,8 +42,30 @@ module.exports=function(grunt){
 	grunt.registerTask('default', ['uglify']);
 
 	//define a simple tasks
-
 	grunt.registerTask('firstSimpleTask', 'This is a simple grunt task', function(){
 		grunt.log.write("this is a simple grunt task..").ok();
 	});	
+
+	//`grunt log:a:b`(the a and b is parameter)
+	grunt.registerTask('log', 'log stuff', function(a, b){
+		grunt.log.writeln(this.name, a, b);
+	});
+
+	//asynchronous task
+	grunt.registerTask('asyncFoo', 'My "async" task', function(){
+		var done = this.async();
+
+		grunt.log.writeln("Processing task....");
+
+		setTimeout(function() {
+			grunt.log.writeln("All done!");
+			done();
+		}, 10000);
+
+		grunt.log.writeln("task end...")
+	});
+
+	grunt.registerTask('access', 'Access log parameter', function(){
+		grunt.log.writeln(grunt.config('log.foo'));	
+	});
 }
---
+++ tasks/uglify.js
@@ -0,0 +1,11 @@
+module.exports={
+	uglify: {
+			options: {
+				banner: '/*! <%= pkg.name%> <%= grunt.template.today("yyyy-mm-dd")%>*/\n'
+			},
+			build: {
+				src: 'src/<%= pkg.name%>.js',
+				dest: 'build/<%= pkg.name%>.min.js'
+			}
+		}
+};
